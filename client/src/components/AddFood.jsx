28f1f0a05c90a02e0711a4146cbc1a9ff4932061
Derek Yang <derek.yang7@gmail.com>
2021-10-03 23:56:54 -0400

    Add css styles

--- client/src/components/AddFood.jsx
+++ client/src/components/AddFood.jsx
@@ -1,9 +1,19 @@
 const AddFood = () => {
     return (
-        <div>
-            <h1>Add Food</h1>
-        </div>
-
+        <>
+            <h3>Add New Food</h3>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="text">Food Item</label>
+                    <input type="text" placeholder="Enter food item..." />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="amount">Number of Servings</label>
+                    <input type="number" placeholder="Enter amount..." />
+                </div>
+                <button className="btn">Add Food</button>
+            </form>
+        </>
     )
 }
 
